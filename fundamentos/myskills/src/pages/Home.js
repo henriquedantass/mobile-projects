481b9dd012182de5430477817243481993d8c879
Henrique Dantas <henrique@hotmail.ph>
2021-11-16 17:27:51 -0300

    utilizando stylesheet

--- fundamentos/myskills/src/pages/Home.js
+++ fundamentos/myskills/src/pages/Home.js
@@ -1,11 +1,61 @@
-import React from "react"
-import {View, Text} from "react-native"
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 
-
-export default function Home () { 
+export default function Home() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Henrique Dantas</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+      />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={[styles.title, { marginTop: 30 }]}>My Skills</Text>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+  },
+  title: {
+    color: "#f1f1f1",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1F1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 12,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  button: {
+    backgroundColor: "#A330F7",
+    fontSize: 18,
+    padding: Platform.OS === "ios" ? 15 : 15,
+    marginTop: 10,
+    alignItems: "center",
+    borderRadius: 7,
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
