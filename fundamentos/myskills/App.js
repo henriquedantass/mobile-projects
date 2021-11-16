d35c3147008adc2619d9a3e2d44995c1bb694c22
Henrique Dantas <henrique@hotmail.ph>
2021-11-16 18:42:12 -0300

    statusbar

--- fundamentos/myskills/App.js
+++ fundamentos/myskills/App.js
@@ -1,9 +1,12 @@
-import React from "react"
-import Home from "./src/pages/Home"
+import React from "react";
+import Home from "./src/pages/Home";
+import { StatusBar } from "react-native";
 
-
-export default function App () { 
+export default function App() {
   return (
-     <Home/>
-  )
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" />
+    </>
+  );
 }
